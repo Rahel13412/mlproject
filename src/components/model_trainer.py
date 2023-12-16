507a8247465778f11067ed089171ba66d15a60d9
Rahel13412 <rahelali13412@gmail.com>
2023-12-16 10:35:09 +0530

    hyperparameter tuning done

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -15,7 +15,7 @@ from  xgboost import XGBRegressor
 from sklearn.metrics import r2_score
 from src.exception import CustomException
 from src.logger import logging 
-
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -32,14 +32,20 @@ def save_object(file_path, obj):
     
 
 
-def evaluate_model(X_train,y_train,X_test,y_test,models):
+def evaluate_model(X_train,y_train,X_test,y_test,models,params):
     try:
         
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            param = params[list(models.keys())[i]]
 
+            gs = GridSearchCV(model, param,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            
             model.fit(X_train,y_train)
 
             y_train_pred = model.predict(X_train)
@@ -76,20 +82,60 @@ class ModelTrainer():
                                              test_array[:,:-1],
                                              test_array[:,-1])
             
-            models ={
-                "Random Forest":RandomForestRegressor(),
-                "Decesion Tree": DecisionTreeRegressor(),
-                "Gradient Boosting" : GradientBoostingRegressor(),
-                "Linear Regression" : LinearRegression(),
-                "K-Neighbors Regressor" : KNeighborsRegressor(),
-                "XGB Regressor": XGBRegressor(),
+            models = {
+                "Random Forest": RandomForestRegressor(),
+                "Decision Tree": DecisionTreeRegressor(),
+                "Gradient Boosting": GradientBoostingRegressor(),
+                "Linear Regression": LinearRegression(),
+                "XGBRegressor": XGBRegressor(),
                 "CatBoosting Regressor": CatBoostRegressor(verbose=False),
-                "AdaBoost Regressor" : AdaBoostRegressor(),
+                "AdaBoost Regressor": AdaBoostRegressor(),
+            }
+            
+            
+
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
                 }
+                
+            }
+
             
             
             model_report: dict=evaluate_model(X_train= X_train,y_train=y_train,
-                                              X_test= X_test,y_test=y_test,models=models)
+                                              X_test= X_test,y_test=y_test,models=models,params=params)
             
 
             # to get best model score from dict 
